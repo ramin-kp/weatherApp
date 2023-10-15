38101af194a6bcf23068fca11fa4134da32cf8ec
Ramin Karimpor <ramin.kp81@gmail.com>
2023-10-15 13:56:19 +0330

    show weather city and styling

--- app.js
+++ app.js
@@ -2,13 +2,39 @@ const BASE_URL = "https://api.openweathermap.org/data/2.5";
 const API_KEY = "a02276d2ec788a78ac748aa319792031";
 const searchInput = document.querySelector(".header__search-input");
 const searchBtn = document.querySelector(".header__btn");
+const showWeatherCity = document.querySelector(".weather");
+const cityWeatherInfo = (data) => {
+  console.log(data);
+  const dataInfoJsx = `
+  <div class="weather__city">
+    <h1>${data.name} , ${data.sys.country}</h1>
+  </div>
+  <div class="weather__title">
+  <img class="weather__img" src="https://openweathermap.org/img/wn/${
+    data.weather[0].icon
+  }.png" alt="weather icon" />
+  <h3 class="weather__main">${data.weather[0].main}</h3>
+  <h3 class="weather__temp">${Math.round(data.main.temp)} °C</h3>
+  </div>
+  <div class="weather__indicator">
+    <h3 class="weather__humidity">رطوبت: <span class="weather__humidity-text">%${
+      data.main.humidity
+    }</span></h3>
+    <h3 class="weather__speed">باد: <span class="weather__speed-text">${
+      data.wind.speed
+    }m/s</span></h3>
+  </div>
 
+    `;
+  showWeatherCity.classList.add("weather--show");
+  showWeatherCity.innerHTML = dataInfoJsx;
+};
 const getCurrentWeatherCity = async (city) => {
   const getApi = await fetch(
     `${BASE_URL}/weather?q=${city}&appid=${API_KEY}&units=metric`
   );
   const data = await getApi.json();
-  console.log(data);
+  return data;
 };
 const searchHandler = async () => {
   const cityName = searchInput.value;
@@ -16,6 +42,7 @@ const searchHandler = async () => {
     alert("لطفا نام شهر خود را وارد کنید.");
   }
   const wetherCity = await getCurrentWeatherCity(cityName);
-  console.log(wetherCity);
+  const weatherInfo = cityWeatherInfo(wetherCity);
+  console.log(weatherInfo);
 };
 searchBtn.addEventListener("click", searchHandler);
