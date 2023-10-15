f56b16d19979356ded31ba0133188b11fb4c909a
Ramin Karimpor <ramin.kp81@gmail.com>
2023-10-15 12:27:45 +0330

    featching data for weather Api

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
-const BASE_URL =
-  "https://api.openweathermap.org/data/2.5/weather?q=tehran&appid=a02276d2ec788a78ac748aa319792031";
-
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
 const API_KEY = "a02276d2ec788a78ac748aa319792031";
+const searchInput = document.querySelector(".header__search-input");
+const searchBtn = document.querySelector(".header__btn");
+
+const getCurrentWeatherCity = async (city) => {
+  const getApi = await fetch(
+    `${BASE_URL}/weather?q=${city}&appid=${API_KEY}&units=metric`
+  );
+  const data = await getApi.json();
+  console.log(data);
+};
+const searchHandler = async () => {
+  const cityName = searchInput.value;
+  if (!cityName) {
+    alert("لطفا نام شهر خود را وارد کنید.");
+  }
+  const wetherCity = await getCurrentWeatherCity(cityName);
+  console.log(wetherCity);
+};
+searchBtn.addEventListener("click", searchHandler);
