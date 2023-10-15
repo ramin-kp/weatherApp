d24acbe60ab4feb2f3fab4487ac3bdcb5d09b5cd
Ramin Karimpor <ramin.kp81@gmail.com>
2023-10-15 22:44:49 +0330

    feat: get forecast weather

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const API_KEY = "a02276d2ec788a78ac748aa319792031";
 const searchInput = document.querySelector(".header__search-input");
 const searchBtn = document.querySelector(".header__btn");
 const showWeatherCity = document.querySelector(".weather");
+const showWeatherForecast = document.querySelector(".forecast");
 const searchLocationIcon = document.querySelector(".header__img");
 const cityWeatherInfo = (data) => {
   const dataInfoJsx = `
@@ -42,12 +43,49 @@ const getCurrentWeatherLocation = async (lat, lon, callback) => {
   const data = await getApi.json();
   callback(data);
 };
+const weekDay = (data) => {
+  const weekIndex = new Date(data * 1000).getDay();
+  const day = [
+    "یکشنبه",
+    "دوشنبه",
+    "سه‌شنبه",
+    "چهارشنبه",
+    "پنج‌شنبه",
+    "جمعه",
+    "شنبه",
+  ];
+  return day[weekIndex];
+};
+const setDataForecast = async (data) => {
+  const forecastWeather = await data.list.filter((item) =>
+    item.dt_txt.endsWith("12:00:00")
+  );
+  forecastWeather.forEach((item) => {
+    const forecastWeatherJsx = `<div class="forecast__box">
+        <img class="forecast__img" src="https://openweathermap.org/img/wn/${
+          item.weather[0].icon
+        }.png" alt="" />
+        <h3 class="forecast__day">${weekDay(item.dt)}</h3>
+        <h3 class="forecast__temp">${Math.round(item.main.temp)} °C</h3>
+        <h3 class="forecast__main">${item.weather[0].main}</h3>
+        </div>`;
+    showWeatherForecast.innerHTML += forecastWeatherJsx;
+  });
+};
+const getForecastWeatherLocation = async (city) => {
+  const url = `${BASE_URL}/forecast?q=${city}&appid=${API_KEY}&units=metric`;
+  const getApi = await fetch(url);
+  const data = await getApi.json();
+  setDataForecast(data);
+  console.log(data);
+};
 const searchHandler = async () => {
   const cityName = searchInput.value;
   if (!cityName) {
     alert("لطفا نام شهر خود را وارد کنید.");
   }
   getCurrentWeatherCity(cityName, cityWeatherInfo);
+  getForecastWeatherLocation(cityName);
 };
 const getGeoLocation = () => {};
 const locationHandler = () => {
