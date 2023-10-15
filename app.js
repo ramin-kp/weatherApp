9714ed406b24bd1413c75571f72074681613710e
Ramin Karimpor <ramin.kp81@gmail.com>
2023-10-15 11:58:38 +0330

    header styling

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-console.log("hello world");
+const BASE_URL =
+  "https://api.openweathermap.org/data/2.5/weather?q=tehran&appid=a02276d2ec788a78ac748aa319792031";
+
+const API_KEY = "a02276d2ec788a78ac748aa319792031";
